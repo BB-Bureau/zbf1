7bc95ca8cd105e564e645bff77671f8029e38aa7
Roby <irobyum@gmail.com>
2023-03-04 15:40:59 +1300

    Update constants.js

--- src/program/constants.js
+++ src/program/constants.js
@@ -1,4 +1,4 @@
 // export const clusterUrl = "https://api.devnet.solana.com";
 
 // export const clusterUrl = "http://localhost:3333/solp"
-export const clusterUrl = "http://34.142.210.247/solp"
+export const clusterUrl = "http://35.240.202.129/solp"
